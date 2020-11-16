2eeb94b88022c233c6c4f3b717221986290acb70
Hausos <aurora.makovac@gmail.com>
2020-11-16 03:21:20 +0100

    adding basic chatbot

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ChatBot from 'react-native-chatbot';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ChatBot steps={steps} />
     </View>
   );
 }
+
+const steps = [
+  {
+    id: '1',
+    message: 'hello how are you?',
+    trigger: '2',
+  },
+  {
+    id: '2',
+    message: 'Subscribe',
+    trigger: '3',
+  },
+  {
+    id: '3',
+    user: true,
+    end: true,
+  },
+]
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
